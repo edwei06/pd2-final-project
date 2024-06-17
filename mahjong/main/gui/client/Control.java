b53e564b3c4aef813457bbcdb058bd87cebfb754
edwardwei <ed069218237@gmail.com>
2024-06-17 14:44:19 +0800

    update gui

---
+++ mahjong/main/gui/client/Control.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Control {
+    private JButton chiLowButton, chiMidButton, chiUpButton, pongButton, gangButton, cancelButton, huButton;
+    private GamePanel gamePanel;
+    private int buttonX = 512;
+    private int buttonY = 584;
+
+    public Control(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+        initButton();
+    }
+
+    public void initButton() {
+        chiLowButton = createButton("吃第一張");
+        chiMidButton = createButton("吃中間張");
+        chiUpButton = createButton("吃最後張");
+        pongButton = createButton("碰");
+        gangButton = createButton("槓");
+        cancelButton = createButton("取消");
+        huButton = createButton("胡");
+
+        hideAllButtons();
+
+        gamePanel.add(chiLowButton);
+        gamePanel.add(chiMidButton);
+        gamePanel.add(chiUpButton);
+        gamePanel.add(pongButton);
+        gamePanel.add(gangButton);
+        gamePanel.add(cancelButton);
+        gamePanel.add(huButton);
+    }
+
+    private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideAllButtons();
+                button.setEnabled(true); // 保證按鈕再次顯示時是可用的
+                System.out.println(button.getText() + " 按鈕被點擊並隱藏");
+            }
+        });
+        return button;
+    }
+
+    private void hideAllButtons() {
+        chiLowButton.setVisible(false);
+        chiMidButton.setVisible(false);
+        chiUpButton.setVisible(false);
+        pongButton.setVisible(false);
+        gangButton.setVisible(false);
+        cancelButton.setVisible(false);
+        huButton.setVisible(false);
+    }
+
+    private void disableAllButtons() {
+        chiLowButton.setEnabled(false);
+        chiMidButton.setEnabled(false);
+        chiUpButton.setEnabled(false);
+        pongButton.setEnabled(false);
+        gangButton.setEnabled(false);
+        cancelButton.setEnabled(false);
+        huButton.setEnabled(false);
+    }
+
+    private void enableButton(JButton button) {
+        disableAllButtons(); // 禁用所有按鈕
+        button.setEnabled(true); // 啟用當前按鈕
+    }
+
+    public void showChiLowButton() {
+        chiLowButton.setBounds(buttonX + 80, buttonY, 90, 30);
+        chiLowButton.setVisible(true);
+        enableButton(chiLowButton);
+    }
+
+    public void showChiMidButton() {
+        chiMidButton.setBounds(buttonX + 160, buttonY, 90, 30);
+        chiMidButton.setVisible(true);
+        enableButton(chiMidButton);
+    }
+
+    public void showChiUpButton() {
+        chiUpButton.setBounds(buttonX + 240, buttonY, 90, 30);
+        chiUpButton.setVisible(true);
+        enableButton(chiUpButton);
+    }
+
+    public void showpongButton() {
+        pongButton.setBounds(buttonX + 160, buttonY - 40, 60, 30);
+        pongButton.setVisible(true);
+        enableButton(pongButton);
+    }
+
+    public void showGangButton() {
+        gangButton.setBounds(buttonX + 240, buttonY - 40, 60, 30);
+        gangButton.setVisible(true);
+        enableButton(gangButton);
+    }
+
+    public void showCancelButton() {
+        cancelButton.setBounds(buttonX + 160, buttonY + 40, 60, 30);
+        cancelButton.setVisible(true);
+        enableButton(cancelButton);
+    }
+
+    public void showHuButton() {
+        huButton.setBounds(buttonX + 80, buttonY - 40, 60, 30);
+        huButton.setVisible(true);
+        enableButton(huButton);
+    }
+}
---
+++ mahjong/main/gui/client/Login.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Login extends JPanel implements ActionListener {
+    private JTextField ipAddress;
+    private JTextField portNumber;
+    private JButton join;
+    private GameMainFrame mainFrame;
+
+    public Login(GameMainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setPreferredSize(new Dimension(500, 100));
+
+        ipAddress = new JTextField("localhost");
+        ipAddress.setPreferredSize(new Dimension(100, 30));
+        add(ipAddress);
+
+        portNumber = new JTextField("1234");
+        portNumber.setPreferredSize(new Dimension(100, 30));
+        add(portNumber);
+
+        join = new JButton("Join");
+        join.addActionListener(this);
+        add(join);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == join) {
+            mainFrame.showGamePanel();
+            System.out.println("IP Address: " + ipAddress.getText());
+            System.out.println("Port Number: " + portNumber.getText());
+        }
+    }
+}
